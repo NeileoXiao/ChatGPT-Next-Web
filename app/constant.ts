da7e84cb4b477039499b9d40c98576c1a25a94af
NeileoXiao <neileo@outlook.com>
2023-08-24 17:08:04 +0800

    更新新传界联系方式

--- app/constant.ts
+++ app/constant.ts
@@ -1,11 +1,11 @@
-export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
-export const UPDATE_URL = `${REPO_URL}#keep-updated`;
-export const RELEASE_URL = `${REPO_URL}/releases`;
-export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
-export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
+export const OWNER = "Jcisan";
+export const REPO = "Jcisan AiChat";
+export const REPO_URL = `https://www.jcwiki.net`;
+export const ISSUE_URL = `https://www.jcwiki.net`;
+export const UPDATE_URL = `https://www.jcwiki.net`;
+export const RELEASE_URL = `https://www.jcwiki.net`;
+export const FETCH_COMMIT_URL = `https://www.jcwiki.net`;
+export const FETCH_TAG_URL = `https://www.jcwiki.net`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 export const DEFAULT_API_HOST = "https://chatgpt1.nextweb.fun/api/proxy";
 
